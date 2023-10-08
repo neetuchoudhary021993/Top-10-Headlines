f342960984d5f655285e15dadc802fd78349f6e3
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-10-08 11:50:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import '../styles/App.css';
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState();
-
+  const [loading, setLoading] = useState(true);
+ 
  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY] &max=10&lang=en`);
+        const response = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=YOUR_API_KEY&max=10&lang=en`);
         const data = await response.json();
-        setNews(data.articles);
+        setNewsData(data.articles);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -22,11 +22,13 @@ const App = () => {
 
     fetchData();
   }, [category]);
-
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
   return (
     <div id="main">
       <h1 className='heading'>Top 10 {category} news.</h1>
-      <select value={category}>
+      <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
         <option value="sports">Sports</option>
@@ -35,22 +37,93 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-      <p className='loader'>Loading...</p>
-      <ol>
-        <li key="">
-          <img className='news-img' src="" alt=""/>
+      {loading ? (
+         <p className='loader'>Loading...</p>
+      ) : (
+        <ol>
+          {newsData?.map((article, index) => (
+            <li key={index}>
+          <img className='news-img' src={article.image} alt=""/>
           <section className='new-title-content-author'>
-            <h3 className='news-title'>news title</h3>
+            <h3 className='news-title'>{article.title} </h3>
             <section className='new-content-author'>
-              <p className='news-description'>news description</p>
-              <p className='news-source'><strong>Source:</strong> source name</p>
+              <p className='news-description'>{article.description}</p>
+              <p className='news-source'><strong>Source:</strong>{article.source.name}</p>
             </section>
           </section>
         </li>
+          ))}
+        
       </ol>
+      )}
+     
+      
     </div>
   )
 }
 
 
 export default App;
+
+
+
+
+// import React, { useState, useEffect } from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+//   const [category, setCategory] = useState("general");
+//   const [newsData, setNewsData] = useState([]);
+//   const [loading, setLoading] = useState();
+
+//  useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY] &max=10&lang=en`);
+//         const data = await response.json();
+//         setNewsData(data.articles);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [category]);
+  
+//   const handleCategoryChange = (event) => {
+//     setCategory(event.target.value);
+//   };
+
+//   return (
+//     <div id="main">
+//       <h1 className='heading'>Top 10 {category} news.</h1>
+//       <select value={category}>
+//         <option value="general">General</option>
+//         <option value="business">Business</option>
+//         <option value="sports">Sports</option>
+//         <option value="technology">Technology</option>
+//         <option value="world">World</option>
+//         <option value="entertainment">Entertainment</option>
+//         <option value="science">Science</option>
+//       </select>
+//       <p className='loader'>Loading...</p>
+//       <ol>
+//         <li key="">
+//           <img className='news-img' src="" alt=""/>
+//           <section className='new-title-content-author'>
+//             <h3 className='news-title'>news title</h3>
+//             <section className='new-content-author'>
+//               <p className='news-description'>news description</p>
+//               <p className='news-source'><strong>Source:</strong> source name</p>
+//             </section>
+//           </section>
+//         </li>
+//       </ol>
+//     </div>
+//   )
+// }
+
+
+// export default App;
