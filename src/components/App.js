f9dd7c9d5338fb139f241173823a4d2bd9d2caf9
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-10-08 16:50:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 
-import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
 
+import React, { useState, useEffect } from "react";
+import '../styles/App.css';
 const App = () => {
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
   const [loading, setLoading] = useState(true);
- 
- useEffect(() => {
+  const apikey = "c660a756a07d51e763978742c3e6b7d6";
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=YOUR_API_KEY&max=10&lang=en`);
+        const response = await fetch(
+          "https://gnews.io/api/v4/top-headlines?category=" +
+            category +
+            "&lang=en&country=us&max=10&apikey=" +
+            apikey
+        );
         const data = await response.json();
         setNewsData(data.articles);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
@@ -25,9 +30,10 @@ const App = () => {
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   };
+  console.log(newsData);
   return (
     <div id="main">
-      <h1 className='heading'>Top 10 {category} news.</h1>
+      <h1 className="heading">Top 10 {category} news.</h1>
       <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
@@ -38,92 +44,30 @@ const App = () => {
         <option value="science">Science</option>
       </select>
       {loading ? (
-         <p className='loader'>Loading...</p>
+        <p className="loader">Loading...</p>
       ) : (
         <ol>
           {newsData?.map((article, index) => (
             <li key={index}>
-          <img className='news-img' src={article.image} alt=""/>
-          <section className='new-title-content-author'>
-            <h3 className='news-title'>{article.title} </h3>
-            <section className='new-content-author'>
-              <p className='news-description'>{article.description}</p>
-              <p className='news-source'><strong>Source:</strong>{article.source.name}</p>
-            </section>
-          </section>
-        </li>
+              <img className="news-img" src={article.image} alt="" />
+              <section className="new-title-content-author">
+                <h3 className="news-title">{article.title} </h3>
+                <section className="new-content-author">
+                  <p className="news-description">{article.description}</p>
+                  <p className="news-source">
+                    <strong>Source:</strong>
+                    {article.source.name}
+                  </p>
+                </section>
+              </section>
+            </li>
           ))}
-        
-      </ol>
+        </ol>
       )}
-     
-      
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
 
 
-
-
-// import React, { useState, useEffect } from 'react'
-// import '../styles/App.css';
-
-// const App = () => {
-//   const [category, setCategory] = useState("general");
-//   const [newsData, setNewsData] = useState([]);
-//   const [loading, setLoading] = useState();
-
-//  useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY] &max=10&lang=en`);
-//         const data = await response.json();
-//         setNewsData(data.articles);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [category]);
-  
-//   const handleCategoryChange = (event) => {
-//     setCategory(event.target.value);
-//   };
-
-//   return (
-//     <div id="main">
-//       <h1 className='heading'>Top 10 {category} news.</h1>
-//       <select value={category}>
-//         <option value="general">General</option>
-//         <option value="business">Business</option>
-//         <option value="sports">Sports</option>
-//         <option value="technology">Technology</option>
-//         <option value="world">World</option>
-//         <option value="entertainment">Entertainment</option>
-//         <option value="science">Science</option>
-//       </select>
-//       <p className='loader'>Loading...</p>
-//       <ol>
-//         <li key="">
-//           <img className='news-img' src="" alt=""/>
-//           <section className='new-title-content-author'>
-//             <h3 className='news-title'>news title</h3>
-//             <section className='new-content-author'>
-//               <p className='news-description'>news description</p>
-//               <p className='news-source'><strong>Source:</strong> source name</p>
-//             </section>
-//           </section>
-//         </li>
-//       </ol>
-//     </div>
-//   )
-// }
-
-
-// export default App;
